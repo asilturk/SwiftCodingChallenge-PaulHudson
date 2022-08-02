480dbe641ee76709d6b94ea0e499f181c97fdceb
burak furkan asilturk <dev.asilturk@gmail.com>
2022-08-02 22:37:32 +0300

    add challange

--- SwiftCodingChallenge-PaulHudson.playground/Pages/Challenge 1 - Are the letters unique?.xcplaygroundpage/Contents.swift
+++ SwiftCodingChallenge-PaulHudson.playground/Pages/Challenge 1 - Are the letters unique?.xcplaygroundpage/Contents.swift
@@ -1,13 +1,28 @@
-import UIKit
+/**
+
+ Write a function that accepts a String as its only parameter, and returns true if the string has
+ only unique letters, taking letter case into account.
+ Sample input and output
+ • The string “No duplicates” should return true.
+ • The string “abcdefghijklmnopqrstuvwxyz” should return true.
+ • The string “AaBbCc” should return true because the challenge is case-sensitive.
+ • The string “Hello, world” should return false because of the double Ls and double Os.
+ For this initial challenge I’ll write some test cases for you, so that you have something to use in the future. These four assert() statements should all evaluate to true, and therefore not trigger an error:
+ assert(challenge1(input: "No duplicates") == true, "Challenge 1
+ failed")
+ assert(challenge1(input: "abcdefghijklmnopqrstuvwxyz") == true,
+ "Challenge 1 failed")
+ assert(challenge1(input: "AaBbCc") == true, "Challenge 1
+ failed")
+ assert(challenge1(input: "Hello, world") == false, "Challenge 1
+ failed")
+ 
+ */
 
 var greeting = "Hello, playground"
 var text = "abcdefghijklmnopqrstuvwxyz"
 
-// My Solution
 func isTheLettersUniqe(_ param: String) -> Bool {
-//    let set = Set(Array(param))
-//    return set.count == param.count
-    
     Set(param).count == param.count
 }
 
--- SwiftCodingChallenge-PaulHudson.playground/Pages/Challenge 2 - Is a string a palindrome?.xcplaygroundpage/Contents.swift
+++ SwiftCodingChallenge-PaulHudson.playground/Pages/Challenge 2 - Is a string a palindrome?.xcplaygroundpage/Contents.swift
@@ -1,7 +1,34 @@
-//: [Previous](@previous)
 
-import Foundation
+/**
+ 
+ Write a function that accepts a String as its only parameter, and returns true if the string
+ reads the same when reversed, ignoring case.
+ Sample input and output
+ • The string “rotator” should return true.
+ • The string “Rats live on no evil star” should return true.
+ • The string “Never odd or even” should return false; even though the letters are the
+ same in reverse the spaces are in different places.
+ • The string “Hello, world” should return false because it reads “dlrow ,olleH”
+ backwards.
+ 
+ */
 
-var greeting = "Hello, playground"
+func isPalindrome(_ text: String) -> Bool {
+    let ignored = text.lowercased()
+    return ignored == String(ignored.reversed())
+}
 
-//: [Next](@next)
+isPalindrome("rotator")
+isPalindrome("Never odd or even")
+
+/**
+ 
+ Once you figure that out, you’ll just need to use reversed() to reverse the string’s characters, then compare the reversed array with the character view-as-array, like this:
+ Remember, strings are value types in Swift, which means they compare as equal as long as their contents are identical - it doesn’t matter how they are created. As an analogy, we all know that 2 times 2 is equal to 2 + 2, even though the number 4 was created using different methods. The same is true of Swift’s string: even though one is reversed, the == operator just compares the current value.
+ Finally, make sure you remember that your comparison should ignore string case. This can be done with the lowercased() method on the input string, like this:
+
+ */
+func isPalindrome_Alternative(_ text: String) -> Bool {
+    let ignored = text.lowercased()
+    return ignored.reversed() == Array(ignored)
+}
